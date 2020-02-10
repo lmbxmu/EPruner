b4c09c8670c9d87c9cf47bc2f8ecff6e1e22b361
lishaojie <864589477@qq.com>
2020-02-10 19:42:39 +0800

    add appruner.py

--- appruner.py
+++ appruner.py
@@ -4,12 +4,15 @@ import torch.optim as optim
 from utils.options import args
 from model.resnet import ResBasicBlock
 import utils.common as utils
+import numpy as np
+from thop import profile
 
 import os
 import time
 from data import cifar10
 from importlib import import_module
 from sklearn.cluster import AffinityPropagation
+from sklearn.cluster.affinity_propagation_ import euclidean_distances
 from sklearn.random_projection import SparseRandomProjection
 
 device = torch.device(f"cuda:{args.gpus[0]}") if torch.cuda.is_available() else 'cpu'
@@ -29,9 +32,10 @@ def cluster_weight(weight):
     else:
         raise('The weight dim must be 4!!!')
 
-    cluster = AffinityPropagation()
+    affinity_matrix = -euclidean_distances(A, squared=True)
+    preference = np.min(affinity_matrix, axis=0)
+    cluster = AffinityPropagation(preference=preference)
     cluster.fit(A)
-
     return cluster.labels_, cluster.cluster_centers_
 
 def random_project(weight, channel_num):
@@ -41,6 +45,29 @@ def random_project(weight, channel_num):
     rp = SparseRandomProjection(n_components=channel_num * weight.size(2) * weight.size(3))
     rp.fit(A)
     return rp.transform(A)
+
+def get_flops_params(orimodel, prunemodel):
+    input = torch.randn(1, 3, 32, 32).to(device)
+
+    print('--------------UnPruned Model--------------')
+    oriflops, oriparams = profile(orimodel, inputs=(input,))
+    print('Params: %.2f' % (oriparams))
+    print('FLOPS: %.2f' % (oriflops))
+
+    print('--------------Pruned Model--------------')
+    print('model\'s cfg', prunemodel.cfg)
+    oricfg = []
+    oricfg.extend([16]*9)
+    oricfg.extend([32] * 9)
+    oricfg.extend([64] * 9)
+    print(oricfg)
+    flops, params = profile(prunemodel, inputs=(input,))
+    print('Params: %.2f' % (params))
+    print('FLOPS: %.2f' % (flops))
+
+    print('--------------Retention Ratio--------------')
+    print('Params Retention Ratio: %d/%d (%.2f%%)' % (params, oriparams, 100. * params / oriparams))
+    print('FLOPS Retention Ratio: %d/%d (%.2f%%)' % (flops, oriflops, 100. * flops / oriflops))
 
 def cluster_resnet():
     # cfg = {'resnet56': [9, 9, 9],
@@ -72,6 +99,7 @@ def cluster_resnet():
             prune_state_dict.append(name + '.bn1.running_mean')
 
     model = import_module(f'model.{args.arch}').resnet(args.cfg, cfg).to(device)
+    get_flops_params(origin_model, model)
     if args.init_method == 'other':
         pass
     elif args.init_method == 'centroids':
@@ -187,35 +215,35 @@ def main():
         raise('arch not exist!')
     print('==>Search Done!')
 
-    if len(args.gpus) != 1:
-        model = nn.DataParallel(model, device_ids=args.gpus)
-
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.lr_decay_step, gamma=0.1)
-
-
-    for epoch in range(start_epoch, args.num_epochs):
-        train(model, optimizer, loader.trainLoader, args, epoch, topk=(1, 5) if args.dataset == 'imagenet' else (1, ))
-        scheduler.step()
-        test_acc = test(model, loader.testLoader, topk=(1, 5) if args.dataset == 'imagenet' else (1, ))
-
-        is_best = best_acc < test_acc
-        best_acc = max(best_acc, test_acc)
-
-        model_state_dict = model.module.state_dict() if len(args.gpus) > 1 else model.state_dict()
-
-        state = {
-            'state_dict': model_state_dict,
-            'best_acc': best_acc,
-            'optimizer': optimizer.state_dict(),
-            'scheduler': scheduler.state_dict(),
-            'epoch': epoch + 1,
-            'arch': args.cfg,
-            'cfg': cfg
-        }
-        checkpoint.save_model(state, epoch + 1, is_best)
-
-    logger.info('Best accuracy: {:.3f}'.format(float(best_acc)))
+    # if len(args.gpus) != 1:
+    #     model = nn.DataParallel(model, device_ids=args.gpus)
+    #
+    # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    # scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.lr_decay_step, gamma=0.1)
+    #
+    #
+    # for epoch in range(start_epoch, args.num_epochs):
+    #     train(model, optimizer, loader.trainLoader, args, epoch, topk=(1, 5) if args.dataset == 'imagenet' else (1, ))
+    #     scheduler.step()
+    #     test_acc = test(model, loader.testLoader, topk=(1, 5) if args.dataset == 'imagenet' else (1, ))
+    #
+    #     is_best = best_acc < test_acc
+    #     best_acc = max(best_acc, test_acc)
+    #
+    #     model_state_dict = model.module.state_dict() if len(args.gpus) > 1 else model.state_dict()
+    #
+    #     state = {
+    #         'state_dict': model_state_dict,
+    #         'best_acc': best_acc,
+    #         'optimizer': optimizer.state_dict(),
+    #         'scheduler': scheduler.state_dict(),
+    #         'epoch': epoch + 1,
+    #         'arch': args.cfg,
+    #         'cfg': cfg
+    #     }
+    #     checkpoint.save_model(state, epoch + 1, is_best)
+    #
+    # logger.info('Best accuracy: {:.3f}'.format(float(best_acc)))
 
 if __name__ == '__main__':
     main()
